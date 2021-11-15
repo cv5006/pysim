b4d5c3b0ae6802bd38f76a19d0e405bfb6f0871e
Seo <cv5006@naver.com>
2021-11-15 13:59:36 +0900

    Rename for adding csv export and add Tx filter

--- data_viewer/simple_pcan_data_exporter.py
+++ data_viewer/simple_pcan_data_exporter.py
@@ -1,8 +1,11 @@
 import os
+import csv
 from plotly.graph_objects import Figure, Scatter
+
 
 MSG_NUM_IDX = 0
 TIME_OFFSET_IDX = 1
+TxRx_IDX = 4
 DATA_LEN_IDX = 5
 DATA_START_IDX = 6
 
@@ -13,11 +16,12 @@ def JoinHexStr2Int16(high_byte_str, low_byte_str):
     return val
 
 
-def PlotPCAN_Data(data_file_path, res_file_path):
-    print(f'└ Reading file...')
+def ExtractPCAN_Data(data_file_path):
+    print('└ Reading file...', end='')
     with open(data_file_path, 'r') as f:
         lines = f.readlines()
         nlines = len(lines)
+    print('\r└ Reading file... Done!')
 
     timd, intd = [], []
     for idx, line in enumerate(lines):
@@ -28,9 +32,14 @@ def PlotPCAN_Data(data_file_path, res_file_path):
             nlines -= 1
             continue
 
-        progres = int((idx+1)/nlines*100)
-        print(f'\r└ Joining data... {progres}%', end='')    
+        progress = int((idx+1)/nlines*100)
+        print(f'\r└ Joining data... {progress}%', end='')
         col = line.strip().split()
+
+        if col[TxRx_IDX] != "Rx":
+            # Skip Tx message
+            continue
+
         num_hex_str = int(col[DATA_LEN_IDX])
         num_data = int(num_hex_str/2)
         msg_idx = int(col[MSG_NUM_IDX])-1
@@ -41,18 +50,33 @@ def PlotPCAN_Data(data_file_path, res_file_path):
             intd.append(int_data)
             timd.append((msg_idx + i/num_data))
     
-    print()
-    print("└ Drwing plot...")
+    print('\r└ Joining data... Done!')
+    return timd, intd
+
+
+def ExportPlotHTML(timd, intd, res_file_path):    
+    print("└ Drawing plot...", end='')
     fig = Figure()
     fig.add_trace(Scatter(x=timd, y=intd))
     fig.update_layout(xaxis_title='time [ms]',
                       yaxis_title='raw data')
+    print('\r└ Drawing plot... Done!')
 
-    print("└ Exporting html...")
+    print("└ Exporting html...", end='')
     fig.write_html(res_file_path)
+    print("\r└ Exporting html... Done!")
 
-    print('└ Done!')
-    print()
+
+def ExportCSV(timd, intd, res_file_path):    
+    N = len(timd)
+    with open(res_file_path, 'w', newline='') as f:
+        wcsv = csv.writer(f)
+        for i, t in enumerate(timd):
+            progress = int((i+1)/N*100)
+            print(f'\r└ Exporting csv... {progress}%', end='')
+            wcsv.writerow([t, intd[i]])
+
+    print(f'\r└ Exporting csv... Done!')
 
 
 def main():
@@ -64,6 +88,9 @@ def main():
         print(f'"{data_file_root}" folder not found')
 
     else:
+        export_csv = input("Export CSV? y/n: ") == 'y'
+        print()
+
         if not os.path.isdir(res_file_root):
             os.mkdir(res_file_root)
 
@@ -74,15 +101,24 @@ def main():
         for i, f in enumerate(data_files):
             print('---------------------------------')
             print(f'{i+1}/{N} Processing "{f}"!')
-            rf = f.split('.')[0] + '.html'            
+            html_file_name = f.split('.')[0] + '.html'
+            csv_file_name = f.split('.')[0] + '.csv'
+
             data_file_path = os.path.join(data_file_root, f)
-            res_file_path =  os.path.join(res_file_root, rf)
+            html_file_path = os.path.join(res_file_root, html_file_name)
+            csv_file_path  = os.path.join(res_file_root, csv_file_name)
             
-            if os.path.isfile(res_file_path):
+            
+            res_exist = os.path.isfile(csv_file_path) if export_csv else os.path.isfile(html_file_path)
+            if res_exist:
                 print('└ Result already exsist. Skip!')
                 print()
             else:
-                PlotPCAN_Data(data_file_path, res_file_path)
+                timd, intd = ExtractPCAN_Data(data_file_path)
+                ExportPlotHTML(timd, intd, html_file_path)
+                if export_csv:
+                    ExportCSV(timd, intd, csv_file_path)
+                print()
     
     os.system('pause')
 
